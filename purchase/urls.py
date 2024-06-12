9e9c03669dc2c59c9353827daf9f1483882bec9d
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-06-12 22:34:08 +0100

    Fixed pat for payment_form so that it is now visable

--- purchase/urls.py
+++ purchase/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PaymentView, initiate_payment, payment_success, payment_failure
 
 urlpatterns = [
-    path('payments/', PaymentView.as_view(), name='payments'),
+    path('form/', PaymentView.as_view(), name='payment_form'),
     path('initiate/<int:workshop_id>/', initiate_payment, name='initiate_payment'),
     path('success/', payment_success, name='payment_success'),
     path('failure/', payment_failure, name='payment_failure'),
