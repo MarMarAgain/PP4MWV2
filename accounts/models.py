a3182634d23771c925d68b204d542308f88e60be
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-06-14 21:10:59 +0100

    added edit profile section for users with account. Static folder and basic css file for edit_profile added.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    school_details = models.TextField(blank=True)
+    # Add more fields as per your design
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
+from django.db import models
+
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 # accounts/urls.py
 from django.urls import path
-
 from .views import SignUpView
+from . import views
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
 ]
