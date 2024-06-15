7c4be8e0382c3fe3e15cf311c1f6e87684cf8210
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-06-15 12:35:52 +0100

    Edit_profile- both sections put into forms. Css changes & font awesome added

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     school_details = models.TextField(blank=True)
-    # Add more fields as per your design
+    students_info = models.TextField(blank=True)
 
     def __str__(self):
         return f'Profile of {self.user.username}'
