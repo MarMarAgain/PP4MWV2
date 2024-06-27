45ae87fafa6e5f0caf2e3c909e303f13ec39624a
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-06-27 17:23:57 +0100

    Layout for landing page amended (screen size). JS script for burger menu modified

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,26 +1,15 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
     const burgerMenu = document.querySelector('.burger-menu');
     const navLinks = document.querySelector('.nav-links');
 
     burgerMenu.addEventListener('click', function() {
-        navLinks.classList.toggle('show');
+        navLinks.classList.toggle('open'); // Toggle the 'open' class on navLinks
     });
 
     // Close the nav-links if a nav-link is clicked (for mobile view)
     document.querySelectorAll('.nav-link').forEach(item => {
         item.addEventListener('click', () => {
-            navLinks.classList.remove('show');
+            navLinks.classList.remove('open'); // Ensure navLinks is closed on click
         });
     });
-
-    // Ensure nav-links are hidden on larger screens
-    function handleResize() {
-        if (window.innerWidth >= 768) {
-            navLinks.classList.remove('show');
-        }
-    }
-
-    window.addEventListener('resize', handleResize);
 });
