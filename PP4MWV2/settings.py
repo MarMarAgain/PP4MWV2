8d26a3f74de5a2ba86eeef00d9c0af1315aef497
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-06-29 01:15:32 +0100

    Add django-heroku and update settings for Heroku deployment

--- PP4MWV2/settings.py
+++ PP4MWV2/settings.py
@@ -176,7 +176,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 
-
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
