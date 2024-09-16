e237633fa949a5d98e9670ab892b8070cb4577ad
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-09-16 19:41:37 +0100

    staticfile storage added

--- PP4MWV2/settings.py
+++ PP4MWV2/settings.py
@@ -37,6 +37,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
