c2b719a8d06124c1a69e3d390b666021f017aaa9
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-09-18 19:10:16 +0100

    Edit made to stripe webhook and payment_success html

--- purchase/views.py
+++ purchase/views.py
@@ -178,7 +178,7 @@ def payment_failure(request):
 def stripe_webhook(request):
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-    endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
+    endpoint_secret = settings.STRIPE_WH_SECRET
 
     try:
         # Verify the webhook signature
