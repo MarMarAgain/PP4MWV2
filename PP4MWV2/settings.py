f8b69e6fb7fb71daf39b88150cde7251905ec900
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-09-19 21:27:52 +0100

    s3 bucket settings added

--- PP4MWV2/settings.py
+++ PP4MWV2/settings.py
@@ -33,6 +33,24 @@ ALLOWED_HOSTS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Bucket Config
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'masterworkspp5'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    #Static and Media files
+    STATICFILES_STORAGE ='custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
@@ -56,6 +74,7 @@ INSTALLED_APPS = [
     'purchase',
     'workshops',
     'other_pages',
+    'storages',
 
 ]
 
